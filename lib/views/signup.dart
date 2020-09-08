50ba53e61dfaef41b4c6a8265ed513d9946abb09
Daigo <daigo3262@gmail.com>
2020-09-08 16:29:29 +0900

    git confirmation

--- lib/views/signup.dart
+++ lib/views/signup.dart
@@ -85,7 +85,7 @@ class _SignUpState extends State<SignUp> {
                   alignment: Alignment.centerRight,
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 16,vertical: 8),
-                    child: Text("忘れたん?",style: simpleTextStyle(),),
+                    child: Text("password ok?",style: simpleTextStyle(),),
                   ),
                 ),
                 SizedBox(height: 8,),
